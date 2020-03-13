ce734d4339486910f3fb71c33210d7a6ecce60ee
Dennisovich-03 <54091928+Dennisovich-03@users.noreply.github.com>
2020-03-13 23:47:25 +0530

    uncleaned

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
+function navbar() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 var loc = 11;
 var temp;
 var cnt = 0;
 var load = [
   [
-    [4, 3, 5, 2, 6, 9, 7, 8, 0],
-    [6, 8, 2, 5, 7, 1, 4, 9, 3],
-    [1, 9, 7, 8, 3, 4, 5, 6, 2],
-    [8, 2, 6, 1, 9, 5, 3, 4, 7],
-    [3, 7, 4, 6, 8, 2, 9, 1, 5],
-    [9, 5, 1, 7, 4, 3, 6, 2, 8],
-    [5, 1, 9, 3, 2, 6, 8, 7, 4],
-    [2, 4, 8, 9, 5, 7, 1, 3, 6],
-    [7, 6, 3, 4, 1, 8, 2, 5, 9]
+    [0, 6, 0, 3, 0, 0, 8, 0, 4],
+    [5, 3, 7, 0, 9, 0, 0, 0, 0],
+    [0, 4, 0, 0, 0, 6, 3, 0, 7],
+    [0, 9, 0, 0, 5, 1, 2, 3, 8],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [7, 1, 3, 6, 2, 0, 0, 4, 0],
+    [3, 0, 6, 4, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 6, 0, 5, 2, 3],
+    [1, 0, 2, 0, 0, 9, 0, 8, 0]
   ],
   [
-    [4, 3, 5, 2, 6, 0, 7, 8, 0],
-    [6, 8, 2, 5, 7, 1, 4, 9, 3],
-    [1, 9, 7, 8, 3, 4, 5, 6, 2],
-    [8, 2, 6, 1, 9, 5, 3, 4, 7],
-    [3, 7, 4, 6, 8, 2, 9, 1, 5],
-    [9, 5, 1, 7, 4, 3, 6, 2, 8],
-    [5, 1, 9, 3, 2, 6, 8, 7, 4],
-    [2, 4, 8, 9, 5, 7, 1, 3, 6],
-    [7, 6, 3, 4, 1, 8, 2, 5, 9]
-  ],
-  [
-    [0, 3, 5, 2, 6, 9, 7, 8, 0],
-    [6, 8, 2, 5, 7, 1, 4, 9, 3],
-    [1, 9, 7, 8, 3, 4, 5, 6, 2],
-    [8, 2, 6, 1, 9, 5, 3, 4, 7],
-    [3, 7, 4, 6, 8, 2, 9, 1, 5],
-    [9, 5, 1, 7, 4, 3, 6, 2, 8],
-    [5, 1, 9, 3, 2, 6, 8, 7, 4],
-    [2, 4, 8, 9, 5, 7, 1, 3, 6],
-    [7, 6, 3, 4, 1, 8, 2, 5, 9]
+    [0, 2, 0, 4, 5, 6, 7, 8, 9],
+    [4, 5, 7, 0, 8, 0, 2, 3, 6],
+    [6, 8, 9, 2, 3, 7, 0, 4, 0],
+    [0, 0, 5, 3, 6, 2, 9, 7, 4],
+    [2, 7, 4, 0, 9, 0, 6, 5, 3],
+    [3, 9, 6, 5, 7, 4, 8, 0, 0],
+    [3, 9, 6, 5, 7, 4, 8, 0, 0],
+    [0, 4, 0, 6, 1, 8, 3, 9, 7],
+    [7, 6, 1, 0, 4, 0, 5, 2, 8],
+    [9, 3, 8, 7, 2, 5, 0, 6, 0]
   ]
 ];
 
@@ -67,11 +66,17 @@ function getele() {
     }
   }
   if (validate(arr)) solver(arr);
+  else {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+  }
   console.log(arr);
 }
-
-var e1 = document.getElementById("pp");
-var e2 = document.getElementById("bf");
 
 function getelepp() {
   for (var i = 0; i < 9; i++) {
@@ -93,12 +98,12 @@ function getelepp() {
   }
 
   if (validate(arr)) {
-    e1.addEventListener("click", paper(arr));
-    // paper(arr);
+    // e1.addEventListener("click", paper(arr));
+    paper(arr);
   } else {
     var modal = document.getElementById("myModal");
     modal.style.display = "block";
-    window.onclick = function(event) {
+    window.onclick = function (event) {
       if (event.target == modal) {
         modal.style.display = "none";
       }
@@ -229,6 +234,7 @@ async function papersolve() {
       }
     }
   }
+  await paper(arr);
 }
 
 // console.log(poss);
@@ -251,10 +257,11 @@ function aclear() {
 }
 
 function validate(arr) {
+  let count;
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       if (arr[i][j] != 0) {
-        for (let l = 1; l < 9; l++) {
+        for (let l = 0; l < 9; l++) {
           const m = 3 * Math.floor(i / 3) + Math.floor(l / 3);
           const n = 3 * Math.floor(j / 3) + (l % 3);
           if (
@@ -262,11 +269,14 @@ function validate(arr) {
             arr[l][j] == arr[i][j] ||
             arr[m][n] == arr[i][j]
           ) {
-            console.log("Invalid");
-            console.log(i, j, l, arr[i][j], arr[i][l], arr[l][j], arr[m][n]);
-            return false;
+            if (count == 1) {
+              console.log("Invalid");
+              console.log(i, j, l, arr[i][j], arr[i][l], arr[l][j], arr[m][n]);
+              return false;
+            } else count++;
           }
         }
+        // count = 0;
       }
     }
   }
